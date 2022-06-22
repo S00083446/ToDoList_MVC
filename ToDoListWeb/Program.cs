a92a5f195a136d653ac495b2c82d21d333b07507
s00083446 <s00083446@mail.itsligo.ie>
2022-06-22 11:29:29 +0100

    added Roles to App

--- ToDoListWeb/Program.cs
+++ ToDoListWeb/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();;
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 
 // Add services to the container.
